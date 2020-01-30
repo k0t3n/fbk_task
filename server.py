c2e7cad83e3196f68606c212b222ccbc7f4dca5c
k0t3n <k0t3n.mail@gmail.com>
2020-01-30 18:30:36 +0300

    Add main logic

--- server.py
+++ server.py
@@ -1,15 +1,41 @@
 from aiohttp import web
+from gino.ext.aiohttp import Gino
+from sqlalchemy.dialects import postgresql
 
 
 async def handle(request):
-    name = request.match_info.get('name', "Anonymous")
-    text = "Hello, " + name
-    return web.Response(text=text)
+    headers = str(dict(request.headers))
+
+    body = (await request.read()).decode("utf-8") if request.body_exists else None
+    ip = request.remote
+    request_db = await Request.create(body=body, headers=headers, ip=ip)
+    return web.Response(text=f'Request {request_db.id} created')
 
 
-app = web.Application()
-app.add_routes([web.get('/', handle),
-                web.get('/{name}', handle)])
+db = Gino()
+app = web.Application(middlewares=[db])
+app.add_routes([
+    web.post('/', handle),
+])
+
+app['config'] = {'gino': {'dsn': 'postgresql://k0t3n@localhost/fbk_task'}}
+db.init_app(app)
+
+
+class Request(db.Model):
+    __tablename__ = 'requests'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    headers = db.Column(db.Unicode())
+    body = db.Column(db.Unicode())
+    ip = db.Column(postgresql.INET())
+
+
+async def setup_tables(app) -> None:
+    await db.gino.create_all()
+
+
+app.on_startup.append(setup_tables)
 
 if __name__ == '__main__':
     web.run_app(app)
